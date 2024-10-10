beeeb90c9a1d37924fbf6a87a17ea2e70b57df61
Jojhan Perez Arroyave <perezarroyavejojhan@gmail.com>
2024-10-09 19:21:21 -0500

    feat: priority change

--- utils/search_algorithms.py
+++ utils/search_algorithms.py
@@ -107,10 +107,10 @@ def get_neighbors_in_orthogonal_order(pos, model):
     x, y = pos
     # Orden ortogonal: arriba, derecha, abajo, izquierda
     neighbors = [
+        (x - 1, y),  # Izquierda
         (x, y + 1),  # Arriba
         (x + 1, y),  # Derecha
-        (x, y - 1),  # Abajo
-        (x - 1, y)   # Izquierda
+        (x, y - 1)  # Abajo
     ]
     # Filtrar los vecinos válidos que están dentro de los límites del mapa y son caminos
     valid_neighbors = [n for n in neighbors if model.grid.out_of_bounds(n) == False]
